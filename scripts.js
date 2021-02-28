97971f7303e6bb0c9c0ae99749d0babcc2c96afe
ThabetSabha <thabetsabha98@gmail.com>
2021-02-28 06:53:33 +0200

    adjusting ratio

--- scripts.js
+++ scripts.js
@@ -1,77 +1,85 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
-
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
 
 let isRed = false;
 let isRgbSplit = false;
 let isGhosting = false;
-
 
 const toggleRedEffects = () => {
   isRed = !isRed;
   if (isRed) {
     isGhosting = false;
   }
-}
+};
 
 const toggleRgbSplit = () => {
   isRgbSplit = !isRgbSplit;
-}
+};
 
 const toggleGhostingEffects = () => {
   isGhosting = !isGhosting;
   if (isGhosting) {
     isRed = false;
   }
-}
+};
 
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
-      video.srcObject = localMediaStream;             //play video where the source is the webcam
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream; //play video where the source is the webcam
       video.play();
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(`OH NO!!!`, err);
     });
 }
 
 function paintToCanvas() {
-  const width = window.innerWidth > video.videoWidth? video.videoWidth : window.innerWidth;
-  const height = window.innerHeight > video.videoHeight? video.videoHeight : window.innerHeight;
+  let videoRatio = video.videoWidth / video.videoHeight;
+  let windowRatio = window.innerWidth / window.innerHeight;
+  let height;
+  let width;
+
+  //if videoRatio > windowRatio then decrease the canvas height, else decrease the canvas width (to account for ratio);
+  if (videoRatio > windowRatio) {
+    width = window.innerWidth - 20;
+    height = width / videoRatio;
+  } else {
+    height = window.innerHeight - 50;
+    width = videoRatio * height;
+  }
   canvas.width = width;
   canvas.height = height;
+  return setInterval(() => {
+    //updates feed every 16ms (60 frames/second)
+    ctx.drawImage(video, 0, 0, width, height); //takes the image from the video starting from x=0, y=0 and draws it on the canvas
 
-  return setInterval(() => {                         //updates feed every 16ms (60 frames/second)
-    ctx.drawImage(video, 0, 0, width, height);       //takes the image from the video starting from x=0, y=0 and draws it on the canvas
-
-    let pixels = ctx.getImageData(0, 0, width, height);   //gives an array of pixels starting from 0,0 and has the r,b,g,a of each pixel stored.
+    let pixels = ctx.getImageData(0, 0, width, height); //gives an array of pixels starting from 0,0 and has the r,b,g,a of each pixel stored.
 
     if (isRed) {
       pixels = redEffect(pixels);
-
     }
 
     if (isRgbSplit) {
       pixels = rgbSplit(pixels);
-      ctx.globalAlpha = 1;                                //ghosting effect;
+      ctx.globalAlpha = 1; //ghosting effect;
     }
 
     if (isGhosting) {
       ctx.globalAlpha = 0.01;
     }
 
-
     if (!isRgbSplit && !isGhosting) {
       ctx.globalAlpha = 1;
     }
 
     // put them back
-    ctx.putImageData(pixels, 0, 0);                           //display the manipulated pixels
-  }, 1000/60);
+    ctx.putImageData(pixels, 0, 0); //display the manipulated pixels
+  }, 1000 / 60);
 }
 
 function takePhoto() {
@@ -80,18 +88,19 @@ function takePhoto() {
   snap.play();
 
   // take the data out of the canvas
-  const data = canvas.toDataURL('image/jpeg');
-  const link = document.createElement('a');
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
   link.href = data;
-  link.setAttribute('download', 'handsome');                //"handsome" is the name we want our downloaded image to have;
+  link.setAttribute("download", "handsome"); //"handsome" is the name we want our downloaded image to have;
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-  strip.insertBefore(link, strip.firstChild);                //insert link before the first child of strip;
+  strip.insertBefore(link, strip.firstChild); //insert link before the first child of strip;
 }
 
 function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {                       //i+=4 since each pixel is represented with 4 elements rgba
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    //i+=4 since each pixel is represented with 4 elements rgba
     pixels.data[i + 0] = pixels.data[i + 0] + 80; // RED               //pump up red;
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN              
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
@@ -99,7 +108,7 @@ function redEffect(pixels) {
 
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED                 
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
     pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
     pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
@@ -108,4 +117,4 @@ function rgbSplit(pixels) {
 
 getVideo();
 
-video.addEventListener('canplay', paintToCanvas);       // when the webcam is enabled, draw video on canvas
+video.addEventListener("canplay", paintToCanvas); // when the webcam is enabled, draw video on canvas
